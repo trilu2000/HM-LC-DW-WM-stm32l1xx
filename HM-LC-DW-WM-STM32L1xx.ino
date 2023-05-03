c3d7bc8a7ef12815571cfca1aab17c049f97d7c9
trilu2000 <trilu@gmx.de>
2023-05-03 17:25:23 +0200

    some update and implemented an debug option

--- HM-LC-DW-WM-STM32L1xx.ino
+++ HM-LC-DW-WM-STM32L1xx.ino
@@ -4,10 +4,17 @@
 // HM-LC-DW-WM modified for the STM32L1xx CPU by trilu
 //- -----------------------------------------------------------------------------------------------------------------------
 
+#if __has_include("private.h") 
+  #include "private.h"
+#else
+  #define DEVICE_ID {0x12, 0x34, 0x56}
+  #define SERIAL_ID "HB01234567"
+#endif
 
-#define HIDE_IGNORE_MSG
-//#define DIMMER_EXTRA_DEBUG
+//#define HIDE_IGNORE_MSG
+#define DIMMER_EXTRA_DEBUG
 #define RADIOWATCHDOG
+#define RADIO_EXTRA_DEBUG
 //#define NDEBUG
 #undef NDEBUG
 
@@ -38,8 +45,8 @@ using namespace as;
 
 // define all device properties
 const struct DeviceInfo PROGMEM devinfo = {
-  {0x07, 0xED, 0x4A},     // Device ID
-  "HB00519498",           // Device Serial
+  DEVICE_ID,              // Device ID
+  SERIAL_ID,              // Device Serial
   
   {0x01,0x08},            // Device Model: HM-LC-DW-WM dual white LED dimmer
   //{0x00,0x67},          // Device Model: HM-LC-Dim1PWM-CV
@@ -124,8 +131,55 @@ void loop() {
   bool worked = hal.runready();
   bool poll = sdev.pollRadio();
   if( worked == false && poll == false ) {
-    hal.activity.savePower<Idle<true> >(hal);
+    //hal.activity.savePower<Idle<true> >(hal);
   }
 }
 
 
+#ifdef RADIO_EXTRA_DEBUG
+/* Use serial input function to debug CC1101 status at runtime
+*  Input format: <type><info> followed by a return
+*  There are two types available, S = CC1101_STATUS, C = CC1101_CONFIG
+* 
+*  CC1101_STATUS:
+*  CC1101_PARTNUM          0x30               (0x00)  Chip ID
+*  CC1101_VERSION          0x31               (0x04)  Chip ID
+*  CC1101_FREQEST          0x32               (0x00)  Frequency Offset Estimate from Demodulator
+*  CC1101_LQI              0x33               (0x00)  Demodulator Estimate for Link Quality
+*  CC1101_RSSI             0x34               (0x00)  Received Signal Strength Indication
+*  CC1101_MARCSTATE        0x35               (0x00)  Main Radio Control State Machine State
+*  CC1101_WORTIME1         0x36               (0x00)  High Byte of WOR Time
+*  CC1101_WORTIME0         0x37               (0x00)  Low Byte of WOR Time
+*  CC1101_PKTSTATUS        0x38               (0x00)  Current GDOx Status and Packet Status
+*  CC1101_VCO_VC_DAC       0x39               (0x00)  Current Setting from PLL Calibration Module
+*  CC1101_TXBYTES          0x3A               (0x00)  Underflow and Number of Bytes
+*  CC1101_RXBYTES          0x3B               (0x00)  Overflow and Number of Bytes
+*  CC1101_RCCTRL1_STATUS   0x3C               (0x00)  Last RC Oscillator Calibration Result
+*  CC1101_RCCTRL0_STATUS   0x3D               (0x00)  Last RC Oscillator Calibration Result
+*
+*/
+
+RadioSPI spi;
+
+void serialEventRun(void) {
+  if (!DSERIAL.available()) return;
+  String test = DSERIAL.readString();
+  uint8_t info = ((test.charAt(1) - 0x30) * 0x10) + (test.charAt(2) - 0x30);
+
+  if (test.charAt(0) == 's') {
+    if ((info < 0x30) || (info > 0x3D)) return;
+    uint8_t x = spi.readReg(info, CC1101_STATUS);
+    DPRINT('\n'); DPRINT(F("read status: 0x")); DHEX(info); DPRINT(F(": 0x")); DHEX(x); DPRINT('\n');
+
+  }
+  else if (test.charAt(0) == 'c') {
+    DPRINTLN(F("read config"));
+
+  }
+  else if (test.charAt(0) == 's') {
+    DPRINTLN(F("send string"));
+    //test = test
+  }
+
+}
+#endif
